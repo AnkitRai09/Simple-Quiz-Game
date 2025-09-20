0de5348d7377ee750aff5be2ea5d161771e56305
ANKIT RAI <anukit.08.09.2008@gmail.com>
2025-09-20 18:54:40 +0530

    Add files via upload

---
+++ quiz_game.py
@@ -0,0 +1,64 @@
+print("WELCOME TO MY QUIZ")
+
+playing = input("DO YOU WANT TO PLAY QUIZ?  " )
+if playing.lower() != "yes":
+    quit()
+
+answer = input("what does the word ipo stands for?   ")
+if answer.lower() == "initial public offering":
+    print("correct")
+else: 
+    print("incorrect")
+
+answer = input("what does the word https stands for?   ")
+if answer.lower() == "hypertext transfer protocol secure":
+    print("correct")
+else: 
+    print("incorrect")
+
+
+answer = input("what is the another name of arabh shah?   ")
+if answer.lower() == "kana mama":
+    print("correct")
+else: 
+    print("incorrect")
+
+
+answer = input("what is the favorite thing of mantu kr singh?   ")
+if answer.lower() == "fal ka thela":
+    print("correct")
+else: 
+    print("incorrect")
+
+
+
+answer = input("what does the word gpo stands for?   ")
+if answer.lower() == "general post office":
+    print("correct")
+else: 
+    print("incorrect")
+
+
+
+answer = input("who is ceo of google?   ")
+if answer.lower() == "sundar pichai":
+    print("correct")
+else: 
+    print("incorrect")
+
+
+
+answer = input("who is the prime minister of india?   ")
+if answer.lower() == "narendra modi":
+    print("correct")
+else: 
+    print("incorrect")
+
+
+
+answer = input("who is the ceo of microsoft?   ")
+if answer.lower() == "satya nadella":
+    print("correct")
+else: 
+    print("incorrect")
+print("thank you")
